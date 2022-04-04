4a41ab6f5a0fc97bbd36cc712aee4795e82bb7f2
margarcac1 <margarcac1@alum.us.es>
2022-04-04 17:50:53 +0200

    first commit~

---
+++ src/main/java/com/ArbolGenealogico/Arbol/ArbolApplication.java
@@ -0,0 +1,13 @@
+package com.ArbolGenealogico.Arbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArbolGenealogico/Arbol/ArbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArbolGenealogico.Arbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
